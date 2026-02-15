e95e16f552b6b91764481d1e5fd76428d65a4e36
IWTSUICIDE <7iuca.main.03@gmail.com>
2026-02-15 22:53:50 +0300

    Update script.js

--- script.js
+++ script.js
@@ -156,3 +156,12 @@ function drawMatrix() {
     }
 }
 setInterval(drawMatrix, 50);
+
+document.addEventListener('contextmenu', e => e.preventDefault());
+document.onkeydown = function(e) {
+    if (e.keyCode == 123 || 
+        (e.ctrlKey && e.shiftKey && (e.keyCode == 73 || e.keyCode == 67 || e.keyCode == 74)) || 
+        (e.ctrlKey && e.keyCode == 85)) {
+        return false;
+    }
+};
